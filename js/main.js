0ab69d6c55d6608528d9fd69c19db4f4dc7c3771
SarahOh92 <201124081oek@gmail.com>
2022-04-28 19:32:50 +0900

    탭버튼 클릭 시 색깔 수정

--- js/main.js
+++ js/main.js
@@ -31,22 +31,6 @@ $(function () {
         location.href ="mobile/index.html"}
 })
 
-// tab 버튼
-const tabList = document.querySelectorAll('.tab_menu .list li');
-const contents = document.querySelectorAll('.tab_menu .content_area .cont')
-let activeCont = '';
-for(var i = 0; i < tabList.length; i++){
-  tabList[i].querySelector('.btn').addEventListener('click', function(e){
-    e.preventDefault();
-    for(var j = 0; j < tabList.length; j++){
-      tabList[j].classList.remove('is_on');
-      contents[j].style.display = 'none';
-    }
-    this.parentNode.classList.add('is_on');
-    activeCont = this.getAttribute('href');
-    document.querySelector(activeCont).style.display = 'block';
-  });
-}
 $(function(){
 	var visualWrap = $("#brandVisual"),
 	visual = visualWrap.find(".visual_slide>li"),
--- js/tab.js
+++ js/tab.js
@@ -1,18 +1,27 @@
-// const tabList = document.querySelectorAll('.tab_menu .list li');
-// const contents = document.querySelectorAll('.tab_menu .cont_area .cont')
-// let activeCont = '';
-// for(var i = 0; i < tabList.length; i++){
-//   tabList[i].querySelector('.btn').addEventListener('click', function(e){
-//     e.preventDefault();
-//     for(var j = 0; j < tabList.length; j++){
-//       tabList[j].classList.remove('is_on');
-//       contents[j].style.display = 'none';
-//     }
-//     this.parentNode.classList.add('is_on');
-//     activeCont = this.getAttribute('href');
-//     document.querySelector(activeCont).style.display = 'block';
-//   });
-// }
 $( function() {
   $( "#tabs" ).tabs();
 } );
+
+const tabList = document.querySelectorAll('.tab_menu .list li');
+const contents = document.querySelectorAll('.tab_menu .cont_area .cont')
+let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+
+for(var i = 0; i < tabList.length; i++){
+  tabList[i].querySelector('.btn').addEventListener('click', function(e){
+    e.preventDefault();
+    for(var j = 0; j < tabList.length; j++){
+      // 나머지 버튼 클래스 제거
+      tabList[j].classList.remove('is_on');
+
+      // 나머지 컨텐츠 display:none 처리
+      contents[j].style.display = 'none';
+    }
+
+    // 버튼 관련 이벤트
+    this.parentNode.classList.add('is_on');
+
+    // 버튼 클릭시 컨텐츠 전환
+    activeCont = this.getAttribute('href');
+    document.querySelector(activeCont).style.display = 'block';
+  });
+}
